8e1d47099693233472f9d40e707d4a654a47b39c
S-Jaswal <sjasdocs@gmail.com>
2023-12-18 13:18:30 -0500

    Made changes to js

--- script.js
+++ script.js
@@ -14,5 +14,57 @@ function shakeMagic8Ball() {
         return
     }
 
+    const randomNumber = Math.floor(Math.random() * 8);
+    let answer, image, color;
+
+    switch (randomNumber) {
+        case 0:
+            answer = "Yes";
+            image = "yes-image.jpg" //change this!!;
+            color = "text-success";
+            break;
+        case 1:
+            answer = "No";
+            image = "no-image.jpg" //change this!!;
+            color = "text-danger";
+            break;
+        case 2:
+            answer = "Ask again later";
+            image = "later-image.jpg" //change this!!;
+            color = "text-secondary";
+            break;
+        case 3:
+            answer = "Maybe";
+            image = "maybe-image.jpg";//change this!!;
+            color = "text-warning";
+            break;
+        case 4:
+            answer = "Cannot predict now";
+            image = "cannot-predict-image.jpg";//change this!!;
+            color = "text-muted";
+            break;
+        case 5:
+            answer = "Don't count on it";
+            image = "dont-count-on-it-image.jpg";//change this!!;
+            color = "text-danger";
+            break;
+        case 6:
+            answer = "Most likely";
+            image = "most-likely-image.jpg";//change this!!;
+            color = "text-success";
+            break;
+        case 7:
+            answer = "Outlook not so good";
+            image = "outlook-not-good-image.jpg";//change this!!;
+            color = "text-danger";
+            break;
+        default:
+            break;
+    }
+
+    const responseText = document.getElementById('response-text');
+    responseText.innerText = answer;
+    responseText.classList = `display-4 ${color}`;
+    document.getElementById('response-image').src = image;
 
 }
